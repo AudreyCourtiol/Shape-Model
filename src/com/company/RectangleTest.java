dbdfa46ec60f20cb7b839cc002594a6432a8365a
AudreyCourtiol <audrey.courtiol@edu.ece.fr>
2021-10-01 13:54:40 +0200

    tests

--- src/com/company/RectangleTest.java
+++ src/com/company/RectangleTest.java
@@ -2,12 +2,25 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
     @Test
     void display() {
+        //create a rectangle
+        ArrayList<Point> cooRectangle = new ArrayList<Point>();
+
+        cooRectangle.add( new Point(0, 4));
+        cooRectangle.add( new Point(6, 4));
+        cooRectangle.add( new Point(6, 0));
+        cooRectangle.add( new Point(0, 0));
+        Rectangle rec = new Rectangle(cooRectangle);
+
+
     }
 
     @Test
---
+++ src/com/company/TriangleTest.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+    @Test
+    void getCenter() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void getCirconference() {
+    }
+
+    @Test
+    void isPointInShape() {
+    }
+
+    @Test
+    void display() {
+    }
+
+    @Test
+    void moveShape() {
+    }
+}
